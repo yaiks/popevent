b10fbf55836e68617d48c54da3b616ee12b86e24
Ricardo Han <ricardo.han@olxbr.com>
2021-08-15 02:41:00 -0300

    chore: using preact boilerplate

--- src/popup/app.jsx
+++ src/popup/app.jsx
@@ -1,6 +1,23 @@
 import { Logo } from './logo'
 
 export function App(props) {
+  // chrome.storage.onChanged.addListener(changes => {
+  //   for (let [key, { oldValue, newValue }] of Object.entries(changes)) {
+  //     app.innerHTML = `
+  //       <p>${key} - ${newValue.calls}</p>
+  //     `
+  //   }
+  // });
+  
+  // chrome.storage.local.get(null, data => {
+  //   console.log('storage data in popup', data)
+  //   for (const url in data) {
+  //     app.innerHTML = `
+  //       <p>${url} - ${data[url].calls}</p>
+  //     `
+  //   }
+  // });
+
   return (
     <>
       <Logo />
